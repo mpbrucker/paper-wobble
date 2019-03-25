a44bd1a4d95dcfcdbc873649ccea38b062100181
Matthew Brucker <mpbrucker@gmail.com>
2019-03-24 23:47:35 -0400

    Making char rotation happen correctly.

--- js/svg.js
+++ js/svg.js
@@ -1,15 +1,28 @@
+var myPath = project.importSVG(document.getElementById('circle'), {expandShapes: true, applyMatrix: true}).children[0];
+var centerPoint = myPath.bounds.center.clone();
+myPath.pivot = centerPoint;
+console.log(myPath)
+// console.log(centerPoint)
+myPath.position = view.center;
+// myPath.reverse();
+// console.log(myPath.position);
+// console.log(myPath.segments)
+
+// myPath.fullySelected = true;
+
 var CIRCLERADIUS = 200;
 
-var myPath = new Path.Circle({
+var myPath2 = new Path.Circle({
     center: view.center,
     radius: CIRCLERADIUS
 });
+console.log(myPath2)
 
 // myPath.fullySelected = true;
 // Set the pivot point of the circle so it doesn't change
-myPath.pivot = myPath.bounds.center;
 
 var lineText = 'test text';
+var charOffset = myPath.length/lineText.length;
 var textObjects = [];
 
 for (var i = 0; i < lineText.length; i++) {
@@ -22,17 +35,15 @@ for (var i = 0; i < lineText.length; i++) {
     
 
     textObjects[i].pivot = new Point(textObjects[i].bounds.width/2,textObjects[i].bounds.height);
-    textObjects[i].position = myPath.getPointAt(i*100);
+    textObjects[i].position = myPath.getPointAt(i*100)+(myPath.position-centerPoint);
 }
 
 // Add a new point for "wobble" effect
-var newSeg = new Segment(myPath.getLocationAt(myPath.length/8));
-console.log(myPath.getLocationAt(myPath.length/8));
+// var newSeg = new Segment(myPath.getLocationAt(myPath.length/8));
 // newSeg.selected = true;
 // newSeg.handleIn = new Point(-40, 40);
 // newSeg.handleOut = new Point(40, -40);
-myPath.insertSegment(1, newSeg);
-console.log(myPath.segments[1].point)
+// myPath.insertSegment(1, newSeg);
 // myPath.smooth();
 
 var mouseDown = false;
@@ -45,10 +56,13 @@ myPath.strokeColor = 'black';
 
 var prevAngle = -0.01;
 var totalAngle = 0;
+
+var movePoint = myPath.segments[5].point.clone();
 project.view.onMouseMove = function(e) {
     // console.log(myPath.bounds.center);
-
-    var pointDiff = myPath.bounds.center - e.point;
+    // Note that we want to calculate diff from centerPoint, because the center of the bounding box will change
+    var pointDiff = (myPath.position) - e.point;
+    // console.log(pointDiff.length)
     var curAngle = pointDiff.angle;
 
     if (curAngle < 0) {
@@ -60,22 +74,21 @@ project.view.onMouseMove = function(e) {
         myPath.rotate(curAngle-prevAngle);
     }
     totalAngle += curAngle-prevAngle;
-    // console.log(curAngle);
     // console.log(totalAngle);
     // console.log(pointDiff.length)
 
-    if (pointDiff.length > 150) {
-        var pointRad = Math.min(Math.pow((pointDiff.length-150)*0.05, 2)+150, CIRCLERADIUS)
+    if (pointDiff.length > 100) {
+        var pointRad = Math.min((Math.pow((pointDiff.length-100)*0.05, 2)+100)/CIRCLERADIUS, 1);
     } else {
-        var pointRad = Math.min(Math.pow((pointDiff.length-150)*0.25, 2)+150, CIRCLERADIUS)
+        var pointRad = Math.min((Math.pow((pointDiff.length-100)*0.25, 2)+100)/CIRCLERADIUS, 1)
     }
+    // console.log(curAngle)
 
-    myPath.segments[1].point = (pointDiff.normalize()*-pointRad)+myPath.bounds.center;
+    myPath.segments[5].point = (movePoint-centerPoint)*pointRad+centerPoint;
 
     
-    console.log(myPath.length)
+    // console.log(myPath.length)
     if (e.delta !== null) {
-        point = e.delta;
 
         for (var i = 0; i < textObjects.length; i++) {
             var curPos = (i*100) - ((curAngle/360)*myPath.length);
@@ -86,9 +99,9 @@ project.view.onMouseMove = function(e) {
             }
 
             var normAngle = myPath.getNormalAt(curPos).angle;
-
-            textObjects[i].position = myPath.getPointAt(curPos);
-            textObjects[i].rotation = normAngle+90;
+            console.log(myPath.getLocationAt(curPos).point)
+            textObjects[i].position = myPath.getPointAt(curPos).rotate(curAngle-45, centerPoint)+(myPath.position-centerPoint);
+            textObjects[i].rotation = normAngle+45+curAngle;
         }
     }
         prevAngle = curAngle;
